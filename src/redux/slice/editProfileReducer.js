37b16b5111f0b65f546dd4d5df9e86ee811e4aaf
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-01-22 23:22:13 +0100

    WIP-AddModal

--- src/redux/slice/editProfileReducer.js
+++ src/redux/slice/editProfileReducer.js
@@ -2,50 +2,49 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { token } from "../../token";
 
 const initialState = {
-  data: null,
-  status: "idle",
-  error: "",
+	data: null,
+	status: "idle",
+	error: "",
 };
 
 export const editProfile = createAsyncThunk("profile/editProfile", async (dataToEdit, { rejectWithValue }) => {
-  try {
-    const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/`, {
-      method: "PUT",
-      body: { dataToEdit },
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-        // Assicurati di includere qualsiasi header per l'autenticazione se necessario
-      },
-    });
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json(); // Il tuo profilo è ora nel formato corretto
-    return data;
-  } catch (error) {
-    return rejectWithValue(error.message);
-  }
+	try {
+		const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/`, {
+			method: "PUT",
+			body: JSON.stringify(dataToEdit),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		if (!response.ok) {
+			throw new Error("Network response was not ok");
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		return rejectWithValue(error.message);
+	}
 });
 
 const editProfileSlice = createSlice({
-  name: "editProfile",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(editProfile.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(editProfile.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.data = action.payload;
-      })
-      .addCase(editProfile.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.payload;
-      });
-  },
+	name: "editProfile",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(editProfile.pending, (state) => {
+				state.status = "loading";
+			})
+			.addCase(editProfile.fulfilled, (state, action) => {
+				state.status = "success";
+				state.data = action.payload;
+			})
+			.addCase(editProfile.rejected, (state, action) => {
+				state.status = "failed";
+				state.error = action.payload;
+			});
+	},
 });
 
 export const selectEditProfileData = (state) => state.editProfile.data;
