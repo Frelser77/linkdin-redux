447d46cd3fe6ce6832815b99fe604beeb9f8c127
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-01-22 11:03:56 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import NotFound from "./components/NotFound";
+import Profile from "./components/Profile";
+import { Container } from "react-bootstrap";
 
 function App() {
 	return (
-		<Router>
-			<div className="App">
-				<Route path="/" element={Home} />
-				<Route path="/notFound" element={NotFound} />
-			</div>
-		</Router>
+		<div className="App">
+			<BrowserRouter>
+				<Container>
+					<Routes>
+						<Route path="/profile/:userId" element={<Profile />} />
+						<Route path="/notFound" element={<NotFound />} />
+					</Routes>
+				</Container>
+			</BrowserRouter>
+		</div>
 	);
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,1 +1,43 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { Card, Button, ListGroup, ListGroupItem } from "react-bootstrap";
+
+function Profile() {
+	return (
+		<Row>
+			<Col>
+				<Card>
+					<Card.Header className="bg-primary text-white">EPICODE</Card.Header>
+					<Card.Img
+						variant="top"
+						src="path-to-your-image.jpg"
+						style={{
+							width: "100px",
+							height: "100px",
+							borderRadius: "50%",
+							margin: "0 auto",
+							transform: "translateY(-50%)",
+							position: "relative",
+							zIndex: "1000",
+						}}
+					/>
+					<Card.Body>
+						<Card.Title>Salvatore Alessandro D'Amico</Card.Title>
+						<Card.Subtitle className="mb-2 text-muted">Junior Front-end Developer</Card.Subtitle>
+						<Card.Text>Oria, Puglia, Italia</Card.Text>
+						<ListGroup className="list-group-flush">
+							<ListGroupItem>Disponibile a lavorare</ListGroupItem>
+							<ListGroupItem>Ruoli di React Developer</ListGroupItem>
+						</ListGroup>
+						<Button variant="primary">Mostra dettagli</Button>
+					</Card.Body>
+				</Card>
+			</Col>
+			<Col md={2} className="d-none d-md-block">
+				<h1>STEFANO WE LOVE YOU</h1>
+			</Col>
+		</Row>
+	);
+}
+
+export default Profile;
